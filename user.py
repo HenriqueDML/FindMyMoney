b027450c9873af8e144c403d30c2285c2c1ab919
Lucas-Panta <lucas_panta@hotmail.com>
2025-12-21 14:05:22 -0300

    criacao usuario

---
+++ user.py
@@ -0,0 +1,10 @@
+class User():
+    def __init__(self, name, id, birth, income, email, ZipCode, RegisterDate):
+        self.name = name
+        self.id = id
+        self.birth = birth
+        self.income = income
+        self.email = email
+        self.ZipCode = ZipCode
+        self.RegisterDate = RegisterDate
+
