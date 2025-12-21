69db084a2f1ca7914a0546bfbcbb6c025a597097
joao-victor-galvao <joaovictorgalvao1998@gmail.com>
2025-12-21 15:11:53 -0300

    update transactions

--- Entities.py
+++ Entities.py
@@ -1,26 +1,21 @@
 from faker import Faker
+import uuid
+from datetime import datetime
 import random
-fake = Faker('pt_BR')
 
-class User():
-    def __init__(self, name, id, birth, income, email, ZipCode, RegisterDate):
-        self.name = name
-        self.id = id
-        self.birth = birth
-        self.income = income
-        self.email = email
-        self.ZipCode = ZipCode
-        self.RegisterDate = RegisterDate
+fake = Faker('pt_BR')
 
 class Transaction:
     def __init__(self, remetente_id, destinatario_id):
-        self.transaction_id= fake.uuid4()
-        
+        #id da transacao
+        self.transaction_id= str(uuid.uuid4())
+        #remetente e destinatario
         self.sender_id = remetente_id
         self.receiver_id = destinatario_id
-
+        #valor
         self.amount = round(random.uniform(1.00, 10000.00), 2)
-        
+        #Detalhamento da Transação
         self.type = random.choice(["PIX"], ["BOLETO"], ["TED"])
-        self.timestamp = fake.iso8601()
+        self.timestamp = datetime.now().isoformat()
+        self.currency = "BRL"
         self.status = random.choice(["APROVADO"],["NEGADO"])
