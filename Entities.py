1a4cb3e7d9eb09d8fb9d129601e1cadc8d0f7b8e
Lucas-Panta <lucas_panta@hotmail.com>
2025-12-21 16:27:33 -0300

    att entities

--- Entities.py
+++ Entities.py
@@ -12,8 +12,8 @@ class User:
         self.birth = fake.date_of_birth(minimum_age=18, maximum_age=100)
         self.income = random.randint(1,7) #especificar qual renda entra na opções de 1 a 7
         self.email = fake.free_email()
-        self.estate = fake.city()
-        self.city =  fake.estado_nome()
+        self.estate = fake.estado_nome()
+        self.city =  fake.city()
         self.RegisterDate = fake.iso8601()
     
     def para_dict(self):
--- producer_users.py
+++ producer_users.py
@@ -4,10 +4,10 @@ from Entities import User
 
 def rodar_cadastro():
     print("Gerador de Cadastros iniciado...")
-    while True:
+    for _ in range(10):
         novo_user = User()
         dados = novo_user.para_dict()
-        print(f"[CADASTRO] Novo Usuário: {novo_user.name}")
+        print(f"[CADASTRO] Novo Usuário: {novo_user.name}, {novo_user.email}, {novo_user.birth}, {novo_user.income}, {novo_user.city}, {novo_user.estate}")
 
         time.sleep(5)
 
