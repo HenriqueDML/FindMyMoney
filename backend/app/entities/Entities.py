192fe298578ffef9ea78176367c6f8fb3992a5af
joao-victor-galvao <joaovictorgalvao1998@gmail.com>
2026-01-25 23:04:45 -0300

    adaptacao do entities ao novo formato com bd

--- backend/app/entities/Entities.py
+++ backend/app/entities/Entities.py
@@ -23,19 +23,19 @@ class User:
         }
 
 class Transaction:
-    def __init__(self, remetente_id):
+    def __init__(self, remetente_id, destinatario_id):
         #id da transacao
         self.transaction_id= str(uuid.uuid4())
         #remetente e destinatario
         self.sender_id = remetente_id
-        self.receiver_id = fake.company()
+        self.receiver_id = destinatario_id
         #valor
         self.amount = round(random.uniform(1.00, 10000.00), 2)
         #Detalhamento da Transação
         self.type = random.choice(["PIX", "BOLETO", "TED"])
         self.timestamp = datetime.now().isoformat()
         self.currency = "BRL"
-        self.status = random.choice(["APROVADO","NEGADO","EM PROCESSAMENTO"])
+        self.status = random.choice(["APROVADO","NEGADO","EM_PROCESSAMENTO"])
         #Pendente ADICIONAR EMPRESA e destinatario ID
     def para_dict(self):
         return {
